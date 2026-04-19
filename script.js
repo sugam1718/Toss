f14bcdb6de774a2390e2d91bddc2661984db7763
sugam1718 <157233705+sugam1718@users.noreply.github.com>
2026-04-19 17:27:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const topText = document.querySelector(".top");
 function tossCoin() {
   const side = Math.random() < 0.5 ? "heads" : "tails";
 
-  img.style.visibility = "hidden";
   topText.textContent = "Flipping...";
+  img.style.visibility = "hidden";
 
   setTimeout(() => {
-    img.src = `${side}.png`;
+    img.src = side + ".png";
     topText.textContent = `It's ${side}.`;
     img.style.visibility = "visible";
   }, 2000);
